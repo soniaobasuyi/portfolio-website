ae8cd02d96897a3ce5384ec431cb811feec15126
soniaobasuyi <obasuyisonia@gmail.com>
2026-02-16 15:42:03 +0100

    created a custom media query hook

---
+++ src/hooks/useMediaQuery.jsx
@@ -0,0 +1,29 @@
+import { useState, useEffect } from "react";
+
+const useMediaQuery = (query) => {
+    const [matches, setMatches] = useState(() => {
+        if (typeof window !== "undefined") {
+            return window.matchMedia(query).matches;
+        }
+        return false;
+    });
+
+    useEffect(() => {
+        if (typeof window !== "undefined") return;
+
+        const mediaQueryList = window.matchMedia(query);
+        const listener = (event) => {
+            setMatches(event.matches);
+        }
+
+        mediaQueryList.addEventListener("change", listener);
+
+        return () => {
+            mediaQueryList.removeEventListener("change", listener);
+        };
+    }, [query]);
+
+    return matches;
+};
+
+export default useMediaQuery;
