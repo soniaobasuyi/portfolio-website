51a9d645e5cf3e0f69ac46aa46e21cc70f4b0333
soniaobasuyi <obasuyisonia@gmail.com>
2026-03-05 13:11:11 +0100

    completed navbar + navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,37 @@
-function App() {
-  return <h1 className='text-green-400 underline hover:text-3xl'>Hello World! </h1>;
-}
+import { useEffect, useState } from 'react';
+import useMediaQuery from './hooks/useMediaQuery.jsx';
+import Navbar from './scenes/Navbar.jsx';
+import DotGroup from './scenes/DotGroup.jsx';
+
+const App = () => {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const [isTopOfPage, setIsTopOfPage] = useState(true);
+  const isAboveMediaScreens = useMediaQuery('(min-width: 1060px)');
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY === 0) setIsTopOfPage(true);
+      if (window.scrollY !== 0) setIsTopOfPage(false);
+    };
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
+
+  return (
+    <div className="app bg-deep-blue">
+      <Navbar
+        isTopOfPage={isTopOfPage}
+        selectedPage={selectedPage}
+        setSelectedPage={setSelectedPage}
+      />
+
+      <div className="w-5/6 mx-auto md:h-full">
+        {isAboveMediaScreens && (
+          <DotGroup selectedPage={selectedPage} setSelectedPage={setSelectedPage} />
+        )}
+      </div>
+    </div>
+  );
+};
 
 export default App;
